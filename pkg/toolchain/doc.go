f5fc3d99c432b410fb72ac66c39ad19b4c628001
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2022-02-03 09:23:15 +0200

    Create toolchain package

---
+++ pkg/toolchain/doc.go
@@ -0,0 +1,7 @@
+/*
+Package toolchain provides a set of utilities to manage and use
+C/C++ compilers, debuggers and memory checkers. It abstracts their
+usage under convenient interfaces so tools can be interchangeably
+utilized.
+*/
+package toolchain
